29a682ce036155216fa4e2fd6932cdc0f2453a19
MafteiMugurel <mugurel.maftei95@gmail.com>
2024-12-03 16:20:49 +0200

    .

--- sum00/sum00/sum00.cpp
+++ sum00/sum00/sum00.cpp
@@ -1,20 +1,29 @@
-// sum00.cpp : This file contains the 'main' function. Program execution begins and ends there.
+﻿// Să se scrie un program care citeşte de la tastatura două numere naturale şi determină suma lor.
 //
 
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+
+int main() {
+    // Declararea variabilelor pentru cele două numere
+    unsigned int num1, num2;
+
+    // Citirea primului număr
+    cout << "Introduceti primul numar: ";
+    cin >> num1;
+
+    // Citirea celui de-al doilea număr
+    cout << "Introduceti al doilea numar: ";
+    cin >> num2;
+
+    // Calcularea sumei celor două numere
+    unsigned int suma = num1 + num2;
+
+    // Afișarea rezultatului
+    cout << "Suma celor doua numere este: " << suma << endl;
+
+    return 0; // Programul s-a terminat cu succes
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
