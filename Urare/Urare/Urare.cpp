96814045e3c748eb4bd883e94d58ac5ea3eb8c57
MafteiMugurel <mugurel.maftei95@gmail.com>
2024-12-04 11:13:28 +0200

    .

---
+++ Urare/Urare/Urare.cpp
@@ -0,0 +1,28 @@
+﻿// Urare.cpp : În preajma Crăciunului toţi suntem sau redevenim copii.
+
+// Scrie un program care afişează pe ecran o urare pentru cei dragi ţie!
+
+
+
+
+
+#include <iostream>
+#include <string>
+
+int main() {
+    // Mesajul de urare
+    std::string urare =
+        "Sărbători fericite!\n"
+        "Fie ca spiritul Crăciunului să vă umple inimile de bucurie,\n"
+        "lumină și speranță. Crăciun Fericit alături de cei dragi!\n";
+
+    // Afișăm urarea
+    std::cout << "***********************************" << std::endl;
+    std::cout << "*                                 *" << std::endl;
+    std::cout << "*      " << urare << "      *" << std::endl;
+    std::cout << "*                                 *" << std::endl;
+    std::cout << "***********************************" << std::endl;
+
+    return 0;
+}
+
