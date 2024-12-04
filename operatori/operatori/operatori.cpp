17633799f2eb7c5fc0e763b0c9b51bbfd367b454
MafteiMugurel <mugurel.maftei95@gmail.com>
2024-12-04 13:17:26 +0200

    Operatori

---
+++ operatori/operatori/operatori.cpp
@@ -0,0 +1,21 @@
+﻿// operatori.cpp : Se dau 2 numere naturale. Calculați suma, diferenţa, produsul şi câtul lor, în această ordine.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x = 10;
+	int z = 3;
+
+	cout << " suma " << x + z << endl;
+	cout << " scadere " << x - z << endl;
+	cout << " inmultire " << x * z << endl;
+	cout << " impartire " << x / z << endl;
+
+
+	return 0; 
+
+}
+
